b7403bbc122e288584b3e5c705df0ed30b9d4003
Jean Chen <71426750+jeanchen2425@users.noreply.github.com>
2022-02-11 16:34:43 -0500

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,184 @@
+#   Look for #IMPLEMENT tags in this file. These tags indicate what has
+#   to be implemented to complete the warehouse domain.
+
+#   You may add only standard python imports---i.e., ones that are automatically
+#   available on TEACH.CS
+#   You may not remove any imports.
+#   You may not import or otherwise source any of your own files
+import math
+import os  # for time functions
+from search import *  # for search engines
+from sokoban import SokobanState, Direction, PROBLEMS  # for Sokoban specific classes and problems
+
+
+def sokoban_goal_state(state):
+    '''
+  @return: Whether all boxes are stored.
+  '''
+    for box in state.boxes:
+        if box not in state.storage:
+            return False
+    return True
+
+
+def heur_manhattan_distance(state):
+    '''admissible sokoban puzzle heuristic: manhattan distance'''
+    '''INPUT: a sokoban state'''
+    '''OUTPUT: a numeric value that serves as an estimate of the distance of the state to the goal.'''
+    boxes_locations = state.boxes
+    storage_point_locations = state.storage
+    sum_manhattan_distance = 0
+    for box in boxes_locations:
+        curr_min = math.sqrt((state.width ** 2) + (state.height ** 2))
+        for storage_point in storage_point_locations:
+            curr_manhattan_distance = abs(storage_point[0] - box[0]) + abs(storage_point[1] - box[1])
+            if curr_manhattan_distance < curr_min:
+                curr_min = curr_manhattan_distance
+        sum_manhattan_distance += curr_min
+    return sum_manhattan_distance
+
+
+# SOKOBAN HEURISTICS
+def trivial_heuristic(state):
+    '''trivial admissible sokoban heuristic'''
+    '''INPUT: a sokoban state'''
+    '''OUTPUT: a numeric value that serves as an estimate of the distance of the state (# of moves required to get) to the goal.'''
+    count = 0
+    for box in state.boxes:
+        if box not in state.storage:
+            count += 1
+    return count
+
+
+def heur_alternate(state):
+    # IMPLEMENT
+    '''a better heuristic'''
+    '''INPUT: a sokoban state'''
+    '''OUTPUT: a numeric value that serves as an estimate of the distance of the state to the goal.'''
+    boxes_locations = state.boxes
+    storage_point_availability = []
+    for point in state.storage:
+        storage_point_availability.append(point)
+    obstacles = state.boxes
+    sum_manhattan_distance = 0
+    for box in boxes_locations:
+        curr_least_step = math.sqrt((state.width ** 2) + (state.height ** 2))
+        matched_with = ()
+        matched_made = False
+        for storage_point in storage_point_availability:
+            # calculate distance
+            curr_manhattan_distance = abs(storage_point[0] - box[0]) + abs(storage_point[1] - box[1])  # need to change
+
+            # update
+            if curr_manhattan_distance < curr_least_step:
+                curr_least_step = curr_manhattan_distance
+                matched_with = storage_point
+                matched_made = True
+        if matched_made:
+            storage_point_availability.remove(matched_with)
+        sum_manhattan_distance += curr_least_step
+    return sum_manhattan_distance
+
+
+def heur_zero(state):
+    '''Zero Heuristic can be used to make A* search perform uniform cost search'''
+    return 0
+
+
+def fval_function(sN, weight):
+    # IMPLEMENT
+    """
+    Provide a custom formula for f-value computation for Anytime Weighted A star.
+    Returns the fval of the state contained in the sNode.
+    Use this function stub to encode the standard form of weighted A* (i.e. g + w*h)
+
+    @param sNode sN: A search node (containing a SokobanState)
+    @param float weight: Weight given by Anytime Weighted A star
+    @rtype: float
+    """
+
+    # Many searches will explore nodes (or states) that are ordered by their f-value.
+    # For UCS, the fvalue is the same as the gval of the state. For best-first search, the fvalue is the hval of the state.
+    # You can use this function to create an alternate f-value for states; this must be a function of the state and the weight.
+    # The function must return a numeric f-value.
+    # The value will determine your state's position on the Frontier list during a 'custom' search.
+    # You must initialize your search engine object as a 'custom' search engine if you supply a custom fval function.
+    return 0
+
+
+def fval_function_XUP(sN, weight):
+    # IMPLEMENT
+    """
+    Another custom formula for f-value computation for Anytime Weighted A star.
+    Returns the fval of the state contained in the sNode.
+    Use this function stub to encode the XUP form of weighted A* 
+
+    @param sNode sN: A search node (containing a SokobanState)
+    @param float weight: Weight given by Anytime Weighted A star
+    @rtype: float
+    """
+    return 0
+
+
+def fval_function_XDP(sN, weight):
+    # IMPLEMENT
+    """
+    A third custom formula for f-value computation for Anytime Weighted A star.
+    Returns the fval of the state contained in the sNode.
+    Use this function stub to encode the XDP form of weighted A* 
+
+    @param sNode sN: A search node (containing a SokobanState)
+    @param float weight: Weight given by Anytime Weighted A star
+    @rtype: float
+    """
+    return 0
+
+
+def compare_weighted_astars():
+    # IMPLEMENT
+    '''Compares various different implementations of A* that use different f-value functions'''
+    '''INPUT: None'''
+    '''OUTPUT: None'''
+    """
+    This function should generate a CSV file (comparison.csv) that contains statistics from
+    4 varieties of A* search on 3 practice problems.  The four varieties of A* are as follows:
+    Standard A* (Variant #1), Weighted A*  (Variant #2),  Weighted A* XUP (Variant #3) and Weighted A* XDP  (Variant #4).  
+    Format each line in your your output CSV file as follows:
+
+    A,B,C,D,E,F
+
+    where
+    A is the number of the problem being solved (0,1 or 2)
+    B is the A* variant being used (1,2,3 or 4)
+    C is the weight being used (2,3,4 or 5)
+    D is the number of paths extracted from the Frontier (or expanded) during the search
+    E is the number of paths generated by the successor function during the search
+    F is the overall solution cost    
+
+    Note that you will submit your CSV file (comparison.csv) with your code
+    """
+
+    for i in range(0, 3):
+        problem = PROBLEMS[i]
+        for weight in [2, 3, 4, 5]:
+            # you can write code in here if you like
+            pass
+
+
+def anytime_weighted_astar(initial_state, heur_fn, weight=1., timebound=10):
+    # IMPLEMENT
+    '''Provides an implementation of anytime weighted a-star, as described in the HW1 handout'''
+    '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
+    '''OUTPUT: A goal state (if a goal is found), else False'''
+    '''implementation of weighted astar algorithm'''
+
+    return False
+
+
+def anytime_gbfs(initial_state, heur_fn, timebound=10):
+    # IMPLEMENT
+    '''Provides an implementation of anytime greedy best-first search, as described in the HW1 handout'''
+    '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
+    '''OUTPUT: A goal state (if a goal is found), else False'''
+    '''implementation of anytime greedy best-first search'''
+    return False
